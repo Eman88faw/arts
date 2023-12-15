108c6838ebceb2ad1ded7d663cfda209a2afcd16
Eman88faw <132667414+Eman88faw@users.noreply.github.com>
2023-12-15 22:39:24 +0100

    bootstrab
    delete Bib

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,12 +1,6 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
-<script>
-    $(document).ready(function(){
-        $('.dropdown-toggle').dropdown()
-    });
-</script>
+
 
 </html>
-<!-- we are not alowed to use this --- -->
--- pages/browse_artworks.php
+++ pages/browse_artworks.php
@@ -1,30 +1,39 @@
 <?php
 include_once "config.php";
 include 'includes/header.php';
-
+function sortText(){
+    $sortColumn = isset($_GET['column']) ? $_GET['column'] : 'Title';
+    $sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
+    $sortText = "Artists Sorted By <b>$sortColumn</b> Ordered By <b>$sort_order</b>";
+    return $sortText;
+}
 function artists($conn)
 {
+    $sortColumn = isset($_GET['column']) ? $_GET['column'] : 'Title';
     $sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
 
     $output = "";
     $last = "";
 
-    $sql = "SELECT * FROM artworks order by YearOfWork $sort_order";
+    $sql = "SELECT * FROM artworks order by $sortColumn $sort_order";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($result as $row){
-        $ima = "./assets/images/works/medium/0" . $row['ImageFileName'] . ".jpg";
-        if(!file_exists($ima)){
+        if(strlen($row['ImageFileName']) < 6){
+            $ima = "./assets/images/works/medium/0" . $row['ImageFileName'] . ".jpg";
+        }
+        else{
             $ima = "./assets/images/works/medium/" . $row['ImageFileName'] . ".jpg";
         }
 //        var_dump($ima);
 //        $img = Check($ima);
-        $output .= '<div class="col-md-3">
+        $output .= '<div class="col-md-3 mb-3">
                         <div class="item mb-3">
                             <a href="/?page=work&Id=' . $row['ArtWorkID'] . '">
-                                <img class="d-block w-100" src="' . $ima . '" alt="' . $row['Title'] . '"style="width:1000;height:360px">
+                                <img class="d-block w-100" src="' . $ima . '" alt="' . $row['Title'] . '"style="height:250px">
                             </a>
+                            <h6 class="text-center bg-dark p-2 text-white">'.$row['Title'].'</h6>
                         </div>
                     </div>';
     }
@@ -40,16 +49,42 @@ function artists($conn)
     </div>
 </div>
 <div class="container">
-    <div class="row text-end my-3">
-        <span>
-           <span style="line-height: 40px"> SORT :</span>
-            <a href="?page=browse_artworks&order=ASC" class="float-end ml-3">
-                <img src="<?=ASSETS?>images/sort-a-to-z.svg" width="40" alt="">
-            </a>
-            <a href="?page=browse_artworks&order=DESC" class="float-end me-1">
-                 <img src="<?=ASSETS?>images/sort-z-to-a.svg" width="40" alt="">
-            </a>
-        </span>
+
+    <div class="row text-end my-3 bg-light align-items-center border-bottom">
+        <div class="col-9 text-start">
+            <p class="mb-0 text-success"><?= sortText();?></p>
+        </div>
+        <div class="col-3 text-end">
+            <span style="line-height: 40px"> SORT :</span>
+            <?php if(isset($_GET['order']) && strtolower($_GET['order']) == 'asc' || !isset($_GET['order'])):?>
+                <a href="?page=browse_artworks&column=Title&order=DESC" class="float-end ml-3">
+                    Title
+                    <img src="<?= ASSETS ?>images/sort-a-to-z.svg" width="40" alt="">
+                </a>
+                <a href="?page=browse_artworks&column=ArtistId&order=DESC" class="float-end me-1">
+                    Artist
+                    <img src="<?= ASSETS ?>images/sort-a-to-z.svg" width="40" alt="">
+                </a>
+                <a href="?page=browse_artworks&column=YearOfWork&order=DESC" class="float-end me-1">
+                    Year
+                    <img src="<?= ASSETS ?>images/sort-a-to-z.svg" width="40" alt="">
+                </a>
+            <?php else:;?>
+                <a href="?page=browse_artworks&column=Title&order=ASC" class="float-end ml-3">
+                    Title
+                    <img src="<?= ASSETS ?>images/sort-z-to-a.svg" width="40" alt="">
+                </a>
+                <a href="?page=browse_artworks&column=ArtistId&order=ASC" class="float-end me-1">
+                    Artist
+                    <img src="<?= ASSETS ?>images/sort-z-to-a.svg" width="40" alt="">
+                </a>
+                <a href="?page=browse_artworks&column=YearOfWork&order=ASC" class="float-end me-1">
+                    Year
+                    <img src="<?= ASSETS ?>images/sort-z-to-a.svg" width="40" alt="">
+                </a>
+            <?php endif;?>
+        </div>
+
     </div>
     <div class="row">
         <?php echo artists($conn);?>
