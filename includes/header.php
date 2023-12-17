edfbf69eb1956d21b4ab9ab3af8ddbd0fa06a761
Eman88faw <132667414+Eman88faw@users.noreply.github.com>
2023-12-17 17:51:00 +0100

    Update header.php
    remove =>/

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Art Reproductions</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
-    <link rel="stylesheet" href="<?= ASSETS;?>/css/styles.css">
+    <link rel="stylesheet" href="<?= ASSETS;?>css/styles.css">
 </head>
